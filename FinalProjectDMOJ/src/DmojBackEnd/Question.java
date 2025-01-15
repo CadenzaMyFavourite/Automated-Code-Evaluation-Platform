8faf018f13ef2c4c3f1f840853fe2e99e065197a
zjiaq <zjiaq@10.242.172.240>
2025-01-15 11:01:43 -0500

    Merge origin/master
    # Conflicts:
    #	FinalProjectDMOJ/src/DmojFrontEnd/DmojLoginPanel.form

--- FinalProjectDMOJ/src/DmojBackEnd/Question.java
+++ FinalProjectDMOJ/src/DmojBackEnd/Question.java
@@ -4,19 +4,39 @@
  */
 package DmojBackEnd;
 
+import java.util.List;
+
 /**
  *
  * @author zjiaq
  */
-class Question {
-    String questionText;
-    String input;
+//class Question {
+//    String questionText;
+//    String input;
+//    String expectedOutput;
+//
+//    public Question(String questionText, String input, String expectedOutput) {
+//        this.questionText = questionText;
+//        this.input = input;
+//        this.expectedOutput = expectedOutput; 
+//    }
+//}
+class TestCase {
+    String inputs;
     String expectedOutput;
 
-    public Question(String questionText, String input, String expectedOutput) {
-        this.questionText = questionText;
-        this.input = input;
-        this.expectedOutput = expectedOutput; 
+    public TestCase(String inputs, String expectedOutput) {
+        this.inputs = inputs;
+        this.expectedOutput = expectedOutput;
     }
 }
 
+class Question {
+    String questionText;
+    List<TestCase> testCases;
+
+    public Question(String questionText, List<TestCase> testCases) {
+        this.questionText = questionText;
+        this.testCases = testCases;
+    }
+}
